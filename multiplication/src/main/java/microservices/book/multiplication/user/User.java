e25dc5032b57597d0d437c8572364dcb13b70359
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-02-20 15:23:56 -0300

    Adding data layer

--- multiplication/src/main/java/microservices/book/multiplication/user/User.java
+++ multiplication/src/main/java/microservices/book/multiplication/user/User.java
@@ -1,20 +1,33 @@
 package microservices.book.multiplication.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  *  Stores information to identify the user
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String alias;
+
+    public User(final String userAlias) {
+        this(null, userAlias);
+    }
 
 }
